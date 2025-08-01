caa129aaee73087bfc3b9acf7b70237b6b026e0c
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-01 16:24:52 +0530

    Add files via upload

---
+++ right_rotate_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[]={10,20,30,40};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    int i;
+    int tmp = arr[n-1];
+    
+    for(i=n-1; i>0; i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    
+    arr[i] = tmp;
+    
+    
+    for(int j=0;j<n;j++)
+    {
+        printf("%d ",arr[j]);
+    }
+
+    return 0;
+}
