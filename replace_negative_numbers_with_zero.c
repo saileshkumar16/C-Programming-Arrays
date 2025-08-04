82cd9d827a01d62000b95cc744351c8e140dcc85
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-04 13:52:01 +0530

    Add files via upload

---
+++ replace_negative_numbers_with_zero.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    int arr[]={1,2,3,5,6,-1,-2,-3};
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]<0)
+        {
+            arr[i]=0;
+        }
+    }
+    
+      for(int i=0; i<n; i++)
+      {
+          printf("%d ",arr[i]);
+      }
+      
+      return 0;
+}
+    
+    
