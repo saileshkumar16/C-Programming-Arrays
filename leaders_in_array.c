8ea68203a91735b8236d07ab6e83b35cd08ad153
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-28 12:20:17 +0530

    Update leaders_in_array.c

--- leaders_in_array.c
+++ leaders_in_array.c
@@ -36,3 +36,35 @@ int main()
 
     return 0;
 }
+
+------------------------------------------------------------------------------------------------------------------------------------
+
+    Optimal solution:
+
+    // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int arr[]={4,7,1,0};
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    int max = arr[n-1]; //right most element is always the max
+    
+     printf("%d ", max);
+    
+    for(int i=n-2;i>=0 ; i--)
+    {
+        if(arr[i]>max)
+        {
+            printf("%d ",arr[i]); //print the max
+            max = arr[i]; //update the new max
+        }
+    }
+    
+    
+
+    return 0;
+}
+
