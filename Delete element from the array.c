d3c0b28426843aafe15aa9f88b1bc71e3eed6f41
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-09-17 17:36:03 +0530

    Update Delete element from the array.c

--- Delete element from the array.c
+++ Delete element from the array.c
@@ -2,33 +2,25 @@
 
 int main() 
 {
-    int arr[] = {1,20,5,78,30};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {3, 6, 12, 1, 5, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
     
-    int ele = 5;
+    int ele = 3;
     
-    int index = 0;
     for(int i=0; i<n; i++)
     {
         if(arr[i]==ele)
         {
-            index = i;
-            break;
+            for(int j=i; j<n-1; j++)
+            {
+                 arr[j] = arr[j+1]; //shift left
+            }
+            n--; //decrease size
         }
     }
     
-    for(int i = index; i<n-1; i++)
-    {
-        arr[i] = arr[i+1];
-    }
-    
-    n--;
-
-    
-    for(int i=0; i<n; i++)
-    {
-        printf("%d ",arr[i]);
-    }
+        for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
 
     return 0;
 }
