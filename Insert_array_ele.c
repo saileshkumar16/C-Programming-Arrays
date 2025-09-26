5c594f26ec83ed6443ced3939ac8d8f062a0d492
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-09-26 13:05:45 +0530

    Update Insert_array_ele.c

--- Insert_array_ele.c
+++ Insert_array_ele.c
@@ -1,25 +1,29 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
-    int arr[100] = {10, 20, 30, 40, 50};
-    int n = 5;  
+int main()
+{
+    int arr[]={10,20,30,40};
     
-    int pos = 2;   
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
     int ele = 25;
-
-    // Shift elements to the right
-    for (int i = n; i > pos; i--)
-    {
-        arr[i] = arr[i - 1];
-    }
-
-    // Insert element
-    arr[pos] = ele;
-    n++;  // increase size
-
     
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    int pos = 2;
+    
+    
+    for(int i=n; i>pos; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    
+    arr[pos] = ele;
+    n++;
+    
+    
+    for(int j=0; j<n; j++)
+    {
+        printf("%d ",arr[j]);
     }
 
     return 0;
